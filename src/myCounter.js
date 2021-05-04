07c7a788edb53f391f7a9ef80d201545c7b96a82
LeoLacana <wilson17414@gmail.com>
2021-05-04 12:17:53 -0300

    lint req 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,13 +19,11 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
-
-console.log(myCounter());
 
 module.exports = myCounter;
