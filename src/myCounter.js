f203d2ef5f2a2981e8c8257590521f9887c6a0ed
LeoLacana <wilson17414@gmail.com>
2021-05-04 12:11:28 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,15 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
